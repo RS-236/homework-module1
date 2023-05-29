7d56363f3f16a866237cdd5d03b090b45b518297
RS-236 <r.schmid.236@westcliff.edu>
2023-05-29 08:00:54 -0400

    second commit

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -2,5 +2,25 @@
 
 //Function to generate combination of characters
 function generateCode() {
-    
+    //Create variables to store generated codes and the type of characters we want to show as codes
+    var code = ' '; //initialize to null value
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
 }
+
+//Generate character multiple times using a loop
+for (i = 1; i<=8; i++) {
+    var char = Math.random()* str.length;  //random select a character from the variable
+    code += str.charAt(char) //accumulate the generated character into a string of 8
+}
+return code; //return the final accumlated string when loop ends
+
+//Get HTML element to display
+document.getElementById("codes").innerHTML = generatedCode();
+
+//Disable Button
+function disableButton() {
+    document.getElementById("submit").disabled = true;
+}
+
+//Activate function
+disableButton();
